b7b98d4bcd5358100112112ab5373116eb728e66
Sveska <71780870+Sveska@users.noreply.github.com>
2022-12-12 13:03:39 -0500

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj6
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public string Publisher { get; set; }
+        public void Print()
+        {
+            Console.WriteLine(Title + "" + Author + "" + Pages + "" + Publisher);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.DataStructures;
+using System.Linq.Expressions;
+///////////////////////////////////////////////////////////////////////////////
+//
+// Author: Zachary Sveska, Sveska@etsu.edu
+// Course: CSCI-2210-001 - Data Structures
+// Assignment: Proj 6
+// Description: Take in the csv file using Stream Reader then spliting lines to add to book class
+//
+///////////////////////////////////////////////////////////////////////////////
+namespace Proj6
+{
+    internal class Program
+    {
+    /// <summary>
+    /// Main Method will create csv filepath uses stream reader to read csv split csv file to add to book method
+    /// then add to avl tree
+    /// </summary>
+    /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string filePath = "@C:\Users\ZSv3s\Downloads\books.csv";
+            
+            try    
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Book book = new Book();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
